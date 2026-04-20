7bcda1a996ddcf4e8131da6dfcfda433ad4d66a2
UNNuwantha <www.nadun50@gmail.com>
2026-04-20 12:37:44 +0530

    Added authentication features using firebase

---
+++ src/firebase.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth/cordova";
+import { addDoc, collection, getFirestore } from "firebase/firestore/lite";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAhAxg8eZ6mq6gJW13h7jKxCaNfYwJVHt4",
+    authDomain: "netflix-clone-7b6f6.firebaseapp.com",
+    projectId: "netflix-clone-7b6f6",
+    storageBucket: "netflix-clone-7b6f6.firebasestorage.app",
+    messagingSenderId: "720162910733",
+    appId: "1:720162910733:web:bf3be6563bd146bcae5b44"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signUp = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authprovider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async (email, password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = ()=>{
+    signOut(auth);
+}
+
+export {auth, db, login, signUp, logout};
