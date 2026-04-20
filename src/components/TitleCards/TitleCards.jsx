095992b24faf3b9af78f127901a3023b060df1f4
UNNuwantha <www.nadun50@gmail.com>
2026-04-20 07:19:53 +0530

    Display movies using TMDB Api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,21 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/cards_data'
 
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGZjNzJkMmE1MzVhNzE0YzE3MDk2NDM2MmIyZjJlMyIsIm5iZiI6MTc3NjM4ODA1NC4wMTEwMDAyLCJzdWIiOiI2OWUxODdkNjFlY2QyZjQ1NmNmYzM0ODQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._fxDAGyt3RcZTzeAgigpLq28wWSdc7UJBjk41lixqAc'
+    }
+  };
+
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -13,6 +23,12 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results))
+      .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel);
   }, [])
 
@@ -21,10 +37,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500` +card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
