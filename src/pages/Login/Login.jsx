85ed14618ae8d6e2d5aa053e041e9f5fcd18c7f2
UNNuwantha <www.nadun50@gmail.com>
2026-04-20 13:47:00 +0530

    Made the website Responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 import { login, signUp } from '../../firebase'
+import netflix_spinner from '../../assets/netflix_spinner.gif'
 
 const Login = () => {
 
@@ -9,39 +10,45 @@ const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const user_auth = async (event) => {
     event.preventDefault();
+    setLoading(true);
     if (signState === "Sign In") {
       await login(email, password);
     } else {
       await signUp(name, email, password);
     }
+    setLoading(false);
   }
 
   return (
-    <div className='login'>
-      <img src={logo} className='login-logo' alt="" />
-      <div className="login-form">
-        <h1>{signState}</h1>
-        <form>
-          {signState === "Sign Up" ? <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder='Your name' /> : <></>}
-          <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder='Email' />
-          <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder='Password' />
-          <button onClick={user_auth} type='submit'>{signState}</button>
-          <div className="form-help">
-            <div className="remember">
-              <input type="checkbox" />
-              <label htmlFor="">Remember Me</label>
+    loading ? <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div> :
+      <div className='login'>
+        <img src={logo} className='login-logo' alt="" />
+        <div className="login-form">
+          <h1>{signState}</h1>
+          <form>
+            {signState === "Sign Up" ? <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder='Your name' /> : <></>}
+            <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder='Email' />
+            <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder='Password' />
+            <button onClick={user_auth} type='submit'>{signState}</button>
+            <div className="form-help">
+              <div className="remember">
+                <input type="checkbox" />
+                <label htmlFor="">Remember Me</label>
+              </div>
+              <p>Need Help?</p>
             </div>
-            <p>Need Help?</p>
+          </form>
+          <div className="form-switch">
+            {signState === "Sign In" ? <p>New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span></p> : <p>Already have Account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span></p>}
           </div>
-        </form>
-        <div className="form-switch">
-          {signState === "Sign In" ? <p>New to Netflix? <span onClick={() => { setSignState("Sign Up") }}>Sign Up Now</span></p> : <p>Already have Account? <span onClick={() => { setSignState("Sign In") }}>Sign In Now</span></p>}
         </div>
       </div>
-    </div>
   )
 }
 
